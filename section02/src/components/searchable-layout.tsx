63f82b362dd36ad41abf2cb1a2778861bb335978
Clt689 <kx1302@naver.com>
2025-03-28 18:19:01 +0900

    feat: 2.8) 페이지별 레이아웃 설정하기

---
+++ section02/src/components/searchable-layout.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from "next/router";
+import { ReactNode, useEffect, useState } from "react";
+import style from "./searchable-layout.module.css";
+
+export default function SearchableLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const q = router.query.q as string;
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 'React에서 발생한 ChangeEvent 객체 타입인데, HTML의 input element에서 발생한 이벤트 타입이다'라는 의미
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div>
+      <div className={style.searchbar_container}>
+        <input
+          value={search}
+          onKeyDown={onKeyDown}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요 ..."
+        ></input>
+        <button onClick={onSubmit}>검색</button>
+      </div>
+      {children}
+    </div>
+  );
+}
