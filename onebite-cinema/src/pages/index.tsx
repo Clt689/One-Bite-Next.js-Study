aeafec7a34bbc5b8bbf63dd0b09108a7a875df4b
Clt689 <kx1302@naver.com>
2025-04-07 16:49:29 +0900

    feat: 인덱스 페이지 - 3개 추천 영화 SSR 적용

--- onebite-cinema/src/pages/index.tsx
+++ onebite-cinema/src/pages/index.tsx
@@ -5,27 +5,30 @@ import MovieItem from "@/components/movie-item";
 import style from "./index.module.css";
 import fetchMovies from "@/lib/fetch-movies";
 import { InferGetServerSidePropsType } from "next";
+import fetchRandomMovies from "@/lib/fetch-random-movies";
 
 export const getServerSideProps = async () => {
   const allMovies = await fetchMovies();
-  // const recoMovies =
+  const recoMovies = await fetchRandomMovies();
 
   return {
     props: {
       allMovies,
+      recoMovies,
     },
   };
 };
 
 export default function Home({
   allMovies,
+  recoMovies,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={style.reco_container}>
-          {movie.slice(0, 3).map((movie) => (
+          {recoMovies.slice(0, 3).map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
