3ae5562200be4b085ed3cce1041f81ad21371555
Clt689 <kx1302@naver.com>
2025-04-07 17:03:33 +0900

    feat: 서치 페이지 SSR 적용

--- onebite-cinema/src/pages/search/index.tsx
+++ onebite-cinema/src/pages/search/index.tsx
@@ -1,16 +1,29 @@
 import { ReactNode } from "react";
 import SearchableLayout from "../../components/searchable-layout";
-import movie from "@/mock/movie.json";
-import { useRouter } from "next/router";
 import MovieItem from "@/components/movie-item";
 import style from "./index.module.css";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchMovies from "@/lib/fetch-movies";
 
-export default function Page() {
-  const router = useRouter();
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const movies = await fetchMovies(q as string);
 
+  return {
+    props: {
+      movies,
+    },
+  };
+};
+
+export default function Page({
+  movies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
-      {movie.map((movie) => (
+      {movies.map((movie) => (
         <MovieItem key={movie.id} {...movie} />
       ))}
     </div>
