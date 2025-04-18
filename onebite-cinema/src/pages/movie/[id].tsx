75a3d1c42cb456f786990e3f3efa39a9f384e18c
Clt689 <kx1302@naver.com>
2025-04-18 17:09:43 +0900

    feat: book 상세 페이지 & fallback 상태일 때도 설정되게 적용

--- onebite-cinema/src/pages/movie/[id].tsx
+++ onebite-cinema/src/pages/movie/[id].tsx
@@ -1,6 +1,7 @@
 import fetchMovies from "@/lib/fetch-movies";
 import fetchOneMovie from "@/lib/fetch-one-movie";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import style from "./[id].module.css";
 
@@ -34,34 +35,56 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({ movie }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  if (router.isFallback) return "로딩 중입니다";
+  if (router.isFallback)
+    return (
+      <>
+        <Head>
+          <title>한입 씨네마</title>
+          <meta property="og:image" content="/thumbnail.png"></meta>
+          <meta property="og:title" content="한입 씨네마"></meta>
+          <meta
+            property="og:description"
+            content="한입 씨네마에 등록된 모든 영화들을 만나보세요."
+          ></meta>
+        </Head>
+        <div>로딩 중입니다</div>
+      </>
+    );
   if (!movie) return "문제가 발생했습니다 다시 시도하세요";
   const { id, title, subTitle, company, runtime, description, posterImgUrl, releaseDate, genres } =
     movie;
 
   return (
-    <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{ backgroundImage: `url('${posterImgUrl}')` }}
-      >
-        <img src={posterImgUrl} />
-      </div>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={posterImgUrl}></meta>
+        <meta property="og:title" content={title}></meta>
+        <meta property="og:description" content={description}></meta>
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.cover_img_container}
+          style={{ backgroundImage: `url('${posterImgUrl}')` }}
+        >
+          <img src={posterImgUrl} />
+        </div>
 
-      <div className={style.info_container}>
-        <div>
-          <h2>{title}</h2>
+        <div className={style.info_container}>
           <div>
-            {releaseDate} / {genres.join(", ")} / {runtime}분
+            <h2>{title}</h2>
+            <div>
+              {releaseDate} / {genres.join(", ")} / {runtime}분
+            </div>
+            <div>{company}</div>
           </div>
-          <div>{company}</div>
-        </div>
 
-        <div>
-          <div className={style.subTitle}>{subTitle}</div>
-          <div className={style.description}>{description}</div>
+          <div>
+            <div className={style.subTitle}>{subTitle}</div>
+            <div className={style.description}>{description}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
