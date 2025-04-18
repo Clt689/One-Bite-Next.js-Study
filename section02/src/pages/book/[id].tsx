1dcdef9831825d7117cca3ed361a701fb089b12d
Clt689 <kx1302@naver.com>
2025-04-18 16:38:20 +0900

    feat: 2.19) SEO 설정하기

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,22 +1,17 @@
 import fetchOneBook from "@/lib/fetch-one-book";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import style from "./[id].module.css";
 
 export const getStaticPaths = () => {
   return {
-    paths: [
-      { params: { id: "1" } },
-      { params: { id: "2" } },
-      { params: { id: "3" } },
-    ],
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }, { params: { id: "3" } }],
     fallback: true,
   };
 };
 
-export const getStaticProps = async (
-  context: GetStaticPropsContext
-) => {
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
 
@@ -27,32 +22,49 @@ export const getStaticProps = async (
   };
 };
 
-export default function Page({
-  book,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Page({ book }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  if (router.isFallback) return "로딩 중입니다..."
-  
+  if (router.isFallback)
+    return (
+      <>
+        <Head>
+          <title>한입북스</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="한입북스"></meta>
+          <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요"></meta>
+        </Head>
+        <div>로딩 중입니다</div>
+      </>
+    );
+
   if (!book) {
     return "문제가 발생했습니다. 다시 시도하세요.";
   }
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
-    <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
-      >
-        <img src={coverImgUrl} />
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={coverImgUrl} />
+        <meta property="og:title" content={title}></meta>
+        <meta property="og:description" content={description}></meta>
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.cover_img_container}
+          style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        >
+          <img src={coverImgUrl} />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
       </div>
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>
-        {author} | {publisher}
-      </div>
-      <div className={style.description}>{description}</div>
-    </div>
+    </>
   );
 }
